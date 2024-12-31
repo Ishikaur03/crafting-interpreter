c2607c79a6aaeb3649895197f0504afb540b2f5a
Ishikaur03 <ishpreetkaur2001@gmail.com>
2024-12-31 18:01:00 +0530

    quote scan'

--- src/main.c
+++ src/main.c
@@ -25,6 +25,7 @@ enum Token
     LESS_EQUAL,
     GREATER,
     GREATER_EQUAL,
+    STRING,
 
 };
 
@@ -171,6 +172,35 @@ int main(int argc, char *argv[])
                     else
                         printf("GREATER > null\n");
                     break;
+                }
+
+                case'"':{
+                    int start = ++i; //escape " mark
+                    while(i<strlen(file_contents) && file_contents[i] !='"'){
+                        i++;
+                    if (i < strlen(file_contents) && file_contents[i] == '\n')
+                        {
+                            currentLine++;
+                        }
+                    
+                    }
+                    printf("STRING \"");
+                for(int j=0; j<i; j++){
+                    printf("%c", file_contents[j]);
+                }
+                    
+                    printf("\"");
+
+                    for(int j=0; j<i; j++){
+                    printf("%c\n", file_contents[j]);
+                    }
+                    if (i >= strlen(file_contents)) {
+        // Unterminated string error
+        fprintf(stderr, "[line %lld] Error: Unterminated string.\n", currentLine);
+        error = 1;
+        
+    }
+break;
                 }
 
                 case' ':
