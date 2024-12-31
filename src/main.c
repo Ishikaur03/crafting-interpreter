2934fa5c7a63f63dbfbd923879422964bef4a5b0
Ishikaur03 <ishpreetkaur2001@gmail.com>
2024-12-31 18:18:32 +0530

    quote scan'

--- src/main.c
+++ src/main.c
@@ -195,7 +195,7 @@ int main(int argc, char *argv[])
         error = 1;
         
     } else {
-        printf("\"");
+        printf("\" ");
     }
                             
                                                   
