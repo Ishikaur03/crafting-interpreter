47a76b5ecdb863dc546f05e9f935c385cbf1d127
Ishikaur03 <ishpreetkaur2001@gmail.com>
2024-12-31 11:47:19 +0530

    parentheses scan

--- src/main.c
+++ src/main.c
@@ -24,8 +24,16 @@ int main(int argc, char *argv[]) {
 
         // Uncomment this block to pass the first stage
         if (strlen(file_contents) > 0) {
-            fprintf(stderr, "Scanner not implemented\n");
-            exit(1);
+           // fprintf(stderr, "Scanner not implemented\n");
+           // exit(1);
+           for(int i=0; i<strlen(file_contents); i++){
+            if(file_contents[i]=='('){
+                printf("LEFT_PAREN ( null \n");
+            }
+            else if(file_contents[i]==')'){
+                printf("RIGHT_PAREN ) null \n");
+            }
+           }
         } 
         printf("EOF  null\n"); // Placeholder, remove this line when implementing the scanner
         
